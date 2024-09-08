4085d2d915a85d7b026878f90ba13e1698d2b8a7
fatmaerciyas <fatmaerciyas22@gmail.com>
2024-09-08 14:36:38 +0300

    changed root

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Portfolio/", // This should match your GitHub Pages repository path
   build: {
-    outDir: "build", // Change this to 'dist' if you are using the default Vite setup
+    outDir: "build", // Ensure this matches your deploy script
   },
 });
