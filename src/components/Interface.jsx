a1e4cf1c202d8d9713a899c814d05e97480d7299
fatmaerciyas <fatmaerciyas22@gmail.com>
2024-09-10 18:17:45 +0300

    updated_cv_path

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -45,7 +45,7 @@ export const Interface = (props) => {
 
 const AboutSection = (props) => {
   const handleDownload = () => {
-    window.open("/Portfolio/public/cv/Fatma_ErciyasCv.pdf", "_blank");
+    window.open("/public/cv/Fatma_ErciyasCv.pdf", "_blank");
   };
 
   const { setSection } = props;
